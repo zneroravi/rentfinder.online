2120f713410f8684c48fab8809ad2d826dd1430a
Ravi Goswami <zneroravi@gmail.com>
2018-03-01 12:27:27 +0530

    property status

---
+++ admin/includes_admin/update_status_property.php
@@ -0,0 +1,29 @@
+<?php include "../../includes/mongo_connection.php";?>
+<?php
+if (isset($_GET['active'])) {
+	$qid = new MongoId($_GET['id']);
+
+	if($_GET['active']=="1"){
+		
+		$query_invoke = $property_collection->update (array("_id"=>$qid), array('$set' => array("approved" =>"1")));
+		//header("location: ../admin_tenant_view.php");
+
+		
+    }
+
+
+    else if($_GET['active']=="2"){
+    	$query_invoke = $property_collection->update (array("_id"=>$qid), array('$set' => array("approved" =>"0")));
+    }
+	
+
+
+if(!$query_invoke) {
+            
+            echo "FAILED";
+        }
+        else{
+          header("Location:../admin_property_view.php");
+        }
+}
+?>
