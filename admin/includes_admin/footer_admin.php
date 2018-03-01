3cced558e2cbf65e3af3ca1f3367b9e50e928d72
Ravi Goswami <zneroravi@gmail.com>
2018-03-01 12:15:52 +0530

    footer

---
+++ admin/includes_admin/footer_admin.php
@@ -0,0 +1,31 @@
+  <footer class="footer">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 footer-navigation">
+                <h3><img src="../assets/img/RentFinder.png" class="img img-responsive" style="width: 340px"></h3>
+                <p class="links"><a href="#">Home</a><strong> · </strong><a href="#">Blog</a><strong> · </strong><a href="#">Pricing</a><strong> · </strong><a href="#">About</a><strong> · </strong><a href="#">Faq</a><strong> · </strong><a href="#">Contact</a></p>
+                <p class="company-name">Company Name © 2015 </p>
+            </div>
+            <div class="col-md-4 col-sm-6 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">21 Revolution Street</span> Paris, France</p>
+                </div>
+                <div><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p class="footer-center-info email text-left"> +1 555 123456</p>
+                </div>
+                <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                    <p> <a href="#" target="_blank">support@company.com</a></p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+            <div class="col-md-4 footer-about">
+                <h4>About RentFinder</h4>
+                <p> RentFinder is an initiative to build up a strong connection between a tenant and a landlord. Our aim is to provide a common interface using which both a tenant and a landlord can fulfill their needs regarding accomodation.
+                </p>
+                
+            </div>
+        </div>
+    </footer>
+    
+</body>
+
+</html>
---
+++ admin/includes_admin/header_admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin: Rentfinder</title>
+    <link rel="stylesheet" href="components/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="components/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="components/css/styles.css">
+    <link rel="stylesheet" href="components/css/user.css">
+    <link rel="stylesheet" href="components/css/Pretty-Footer.css">
+    <link rel="stylesheet" href="components/css/admin.css">
+    <link rel="stylesheet" href="components/css/user.css">
+    <link rel="stylesheet" href="components/css/styles.css">
+   <link rel="icon" href="components/img/favicon_admin_rf.png" type="image/png" sizes="16x16">
+
+   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+
+<body>
+  <script src="components/js/jquery.min.js"></script>
+    <script src="components/bootstrap/js/bootstrap.min.js"></script>
+
+    
