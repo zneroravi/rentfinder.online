18cc0fff596ce41957a5e710b0299fc02f767d3f
Ravi Goswami <zneroravi@gmail.com>
2018-03-02 01:37:38 +0530

    hf

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+  <footer>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 footer-navigation">
+                <h3><img src="assets/img/RentFinder.png" class="img img-responsive" style="width: 340px"></h3>
+              
+                <p class="company-name">Rentfinder: At your service © 2017 </p>
+            </div>
+            <div class="col-md-4 col-sm-6 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">Lovely Professional University</span> Jalandhar, Punjab</p>
+                </div>
+                <div><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p class="footer-center-info email text-left"> +91 9872385214</p>
+                </div>
+                <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                    <p> <a href="#">admin@rentfinder.online</a></p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+            <div class="col-md-4 footer-about">
+                <h4>About RentFinder</h4>
+                <p> RentFinder is an initiative to build up a strong connection between a tenant and a landlord. Our aim is to provide a common interface using which both a tenant and a landlord can fulfill their needs regarding accomodation.
+                </p>
+                
+            </div>
+        </div>
+    </footer>
+    
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rentfinder: At Your Service</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/user.css">
+    <link rel="stylesheet" href="../assets/css/user.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/Google-Style-Login.css">
+
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/Google-Style-Login.css">
+    <link rel="stylesheet" href="../assets/css/Pretty-Header.css">
+    <link rel="stylesheet" href="../assets/css/Pretty-Registration-Form.css">
+
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Footer.css">
+    <link rel="stylesheet" href="assets/css/create_page.css">
+    <link rel="stylesheet" href="assets/fonts/material-icons.css">
+    <link rel="stylesheet" href="../assets/fonts/material-icons.css">
+     <link rel="stylesheet" href="../assets/css/create_page.css">
+    <link rel="icon" href="assets/img/favicon_rf.png" type="image/png" sizes="16x16">
+
+       
+    
+    
+
+
+
+
+</head>
+
+
